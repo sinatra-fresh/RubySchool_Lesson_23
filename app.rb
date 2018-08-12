020252e9a4bb4e52389b92dc9ea99c4587b78503
Alexandr Zherdev <sinatra.fresh@outlook.com>
2018-08-12 17:01:05 +0600

    Getting parameters from POST query

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 #encoding: utf-8
+#REQUIRE:
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+
+
+#Get запросы:
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
@@ -17,3 +22,17 @@ end
 get '/visit' do
 	erb :visit
 end
+
+#POST запросы:
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@master = params[:master]
+
+	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@master}"
+
+end
+
