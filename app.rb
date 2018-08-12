30c8e1a548cde5881f2bfccfc0a2f2e9cf27384c
Alexandr Zherdev <sinatra.fresh@outlook.com>
2018-08-12 15:50:00 +0600

    Initial code for Lesson 23

---
+++ app.rb
@@ -0,0 +1,19 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/contact' do
+	erb :contact
+end
+get '/visit' do
+	erb :visit
+end
